cfcd768d582939df1e5302a5f7a1ade7e283c65c
Thomas Bariabas <tmpariampas@cognity.gr>
2019-09-05 14:56:15 +0300

    Increased the pagination height

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -24,7 +24,7 @@ class Content extends InstanceClass {
         //The height that is not shown into the screen
         const hiddenHeight = contentRootHeight - window.innerHeight;
         //The height that must fetch the new data from the api.
-        const pagination_height = hiddenHeight - 100;
+        const pagination_height = hiddenHeight - 200;
         /*
             In case that user passed the height that is need to fetch data, and is not currently fetching data fetch
             the next page.
