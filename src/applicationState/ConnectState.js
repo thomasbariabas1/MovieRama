1e4c5fa6d494b504632d42dc66ae6e6ee0ba7390
thomasbariabas1 <thomas.bariabas@gmail.com>
2019-09-06 11:15:45 +0300

    Comments into the application state

--- src/applicationState/ConnectState.js
+++ src/applicationState/ConnectState.js
@@ -1,9 +1,8 @@
 import  State from './State'
 import {isEquals} from "../lib/utils";
 
-
+//Wrapper a class and map the application state and the actions into the context of the class
 export const connect = (mapStateToProps = ()=>({}), dispatchActionCreators = {}) => (ClassToBeConnected) =>{
-
 
      class ConnectedClass extends ClassToBeConnected{
 
@@ -13,32 +12,36 @@ export const connect = (mapStateToProps = ()=>({}), dispatchActionCreators = {})
              this.props ={};
 
              this.updateProps();
-             State.setListeners(this.dispatch.bind(this))
+             State.setListeners(this.stateListener.bind(this))
 
          }
+         //Update the context of the class
          updateProps = () =>{
 
              const oldProps = {...this.props};
 
              const actions = {};
+             //Get the applications state
              const newProps = mapStateToProps(State.getState());
+            //Pass the dispatch function and the getState function into the actions
              Object.keys(dispatchActionCreators).map(actionKey=>{
                  actions[actionKey] = dispatchActionCreators[actionKey](State.dispatch, State.getState)
              });
 
              const props = {...newProps};
-
+             //Map application state to context
              Object.keys(props).map(key=>{
                  this.props[key] = props[key]
              });
+             //Map actions to context
              Object.keys(actions).map(key=>{
                  this[key] = actions[key]
              });
-
+            //Check if the new props are equals with the old one's.
              return !isEquals(oldProps, this.props)
          };
-
-         dispatch = () =>{
+        //This calls when the application state is updated.
+         stateListener = () =>{
              const shouldRerender = this.updateProps();
              if(shouldRerender)
              this.render()
--- src/applicationState/State.js
+++ src/applicationState/State.js
@@ -12,11 +12,12 @@ class State {
         }
     }
 
-
+    //Return the current state
     getState = () => {
         return this.state
     };
 
+    //Combine the reducers creating the initial state of the application
     combineReducers = (reducers) =>{
 
         Object.keys(reducers).map(reducer=>{
@@ -30,11 +31,13 @@ class State {
 
     };
 
+    //Set up the components that listen to the state
     setListeners = (listener) =>{
         this.listeners = [...this.listeners, listener]
 
     };
 
+    //Update the state dependence to the action that dispatched
     dispatch = (action) =>{
         Object.keys(this.reducers).map(reducer=>{
             const state =  this.reducers[reducer](this.state[reducer],action);
@@ -44,6 +47,7 @@ class State {
     }
 }
 
+//If there is any state object, initiate it.
 if(!ApplicationState) {
     ApplicationState = new State();
     ApplicationState.combineReducers({ContentReducer});
